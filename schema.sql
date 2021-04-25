983852a08cd9d1160d96b80d45ab906c9ea0b3d2
Dave W <dave.wisinski@gmail.com>
2021-04-25 14:37:28 -0500

    Updated homework files.

--- schema.sql
+++ schema.sql
@@ -24,17 +24,20 @@ CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE salaries (
+	id SERIAL PRIMARY KEY,
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
